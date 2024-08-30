bf4bb8e03990d37b2970aa286507009488d9e427
Haleema Nasir <145924509+thehaleemanasir@users.noreply.github.com>
2024-08-30 21:19:42 +0100

    GEt inserted New Auto ID

--- src/main/java/com/markcrowe/jdbc/commandline/CountryRepository.java
+++ src/main/java/com/markcrowe/jdbc/commandline/CountryRepository.java
@@ -4,12 +4,15 @@ import com.markcrowe.jdbc.countryvaccinationdata.CountryVaccinationData;
 
 import java.sql.*;
 
+import com.markcrowe.jdbc.DatabaseUtility;
+
 public class CountryRepository {
     private final Connection connection;
 
     public CountryRepository(Connection connection) {
         this.connection = connection;
     }
+
     //task1
     public void searchByIsoCode(String isoCode) throws SQLException {
         String query = "SELECT * FROM country_vaccination_data WHERE iso_code = ? ORDER BY date DESC";
@@ -35,27 +38,25 @@ public class CountryRepository {
     }
 
     //task2
-    public int insertNewRecord(CountryVaccinationData data) throws SQLException {
+    public int insertNewRecord(CountryVaccinationData countryVaccinationData) throws SQLException {
         String query = "INSERT INTO country_vaccination_data (country, iso_code, total_vaccinations, people_vaccinated, " +
                 "people_fully_vaccinated, daily_vaccinations, vaccines, date) VALUES (?, ?, ?, ?, ?, ?, ?, CURDATE())";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
-            preparedStatement.setString(1, data.getCountry());
-            preparedStatement.setString(2, data.getIsoCode());
-            preparedStatement.setInt(3, data.getTotalVaccinations());
-            preparedStatement.setInt(4, data.getPeopleVaccinated());
-            preparedStatement.setInt(5, data.getPeopleFullyVaccinated());
-            preparedStatement.setInt(6, data.getDailyVaccinations());
-            preparedStatement.setString(7, data.getVaccines());
+            preparedStatement.setString(1, countryVaccinationData.getCountry());
+            preparedStatement.setString(2, countryVaccinationData.getIsoCode());
+            preparedStatement.setInt(3, countryVaccinationData.getTotalVaccinations());
+            preparedStatement.setInt(4, countryVaccinationData.getPeopleVaccinated());
+            preparedStatement.setInt(5, countryVaccinationData.getPeopleFullyVaccinated());
+            preparedStatement.setInt(6, countryVaccinationData.getDailyVaccinations());
+            preparedStatement.setString(7, countryVaccinationData.getVaccines());
 
+            preparedStatement.executeUpdate();
             // Get the generated new ID
-            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-                if (generatedKeys.next()) {
-                    int generatedId = generatedKeys.getInt(1);
-                    data.setId(generatedId);
-                    return generatedId;
-                }
-            }
+
+            int newId = DatabaseUtility.getNewId(preparedStatement);
+            countryVaccinationData.setId(newId);
+            return newId;
         }
         throw new SQLException("Creating property failed, no ID obtained.");
     }
@@ -67,12 +68,12 @@ public class CountryRepository {
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
             preparedStatement.setString(1, isoCode);
-            preparedStatement.setDate(2, new java.sql.Date(sDate.getTime()));
-            preparedStatement.setDate(3, new java.sql.Date(eDate.getTime()));
+            preparedStatement.setDate(2, new java.sql.Date(beginDate.getTime()));
+            preparedStatement.setDate(3, new java.sql.Date(endDate.getTime()));
 
-            ResultSet ResultSet = preparedStatement.executeQuery();
-            if (ResultSet.next()) {
-                double average = ResultSet.getDouble(1);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                double average = resultSet.getDouble(1);
                 System.out.printf("Average daily vaccinations: %.2f%n", average);
             } else {
                 System.out.println("No data available for the given range.");

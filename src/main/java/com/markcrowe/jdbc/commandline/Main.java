07453067017d6ed817770ee308a6a62788224d7d
Haleema Nasir <145924509+thehaleemanasir@users.noreply.github.com>
2024-08-30 21:22:34 +0100

    fix names, add defaults auto format

--- src/main/java/com/markcrowe/jdbc/commandline/Main.java
+++ src/main/java/com/markcrowe/jdbc/commandline/Main.java
@@ -13,122 +13,130 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Main
-{
+public class Main {
 
-	DecimalFormat twoDigits = new DecimalFormat("0.00");
-	DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	private final CountryRepository repo;
-	public static void main(String[] args) throws SQLException {
+    DecimalFormat twoDigits = new DecimalFormat("0.00");
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private final CountryRepository repository;
 
-		try
-		{
-			new Main();
-		}
-		catch(ParseException ex)
-		{
-			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-		}
+    public static void main(String[] args) throws SQLException {
 
-	}
+        try {
+            new Main();
+        } catch (ParseException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-	public Main() throws ParseException, SQLException {
+    }
 
-		repo = new CountryRepository(DatabaseUtility.getConnection());
+    public Main() throws ParseException, SQLException {
 
-		Console.displayLine("    Welcome to the Country Vaccination Data 2021 Database Main Menu");
-		Console.displayLine();
+        repository = new CountryRepository(DatabaseUtility.getConnection());
 
-		int choice = 0;
+        Console.displayLine("    Welcome to the Country Vaccination Data 2021 Database Main Menu");
+        Console.displayLine();
 
-		while(choice != -1)
-		{
+        int choice = 0;
 
-			//display menu choices
-			Console.displayLine("1:  Task 1");
-			Console.displayLine("2:  Task 2");
-			Console.displayLine("3:  Task 3");
-			Console.displayLine("4:  Task 4");
-			Console.displayLine("-1: Quit");
-			Console.displayLine();
+        while (choice != -1) {
 
-			//prompt user for their menu choice
-			choice = Console.getInt("Enter Choice: ");
+            //display menu choices
+            Console.displayLine("1:  Task 1");
+            Console.displayLine("2:  Task 2");
+            Console.displayLine("3:  Task 3");
+            Console.displayLine("4:  Task 4");
+            Console.displayLine("-1: Quit");
+            Console.displayLine();
 
-			//evaluate user choice
-			switch(choice)
-			{
+            //prompt user for their menu choice
+            choice = Console.getInt("Enter Choice: ");
 
-				case 1:
-					String isoCode = Console.getString("Enter ISO Code: ");
-					taskOne(isoCode);
-					break;
-				case 2:
-					String country = Console.getString("Enter country: ");
-					isoCode = Console.getString("Enter ISO Code: ");
-					Date now = new Date();
-					int totVacc = Console.getInt("Enter Total Vaccinations: ");
-					int peopVacc = Console.getInt("Enter People Vaccinated: ");
-					int totPeopVacc = Console.getInt("Enter People Fully Vaccinated: ");
-					int dailyVacc = Console.getInt("Enter Daily Vaccinations: ");
-					String vaccines = Console.getString("Enter Vaccines: ");
-					CountryVaccinationData c = new CountryVaccinationData(choice, country, isoCode, now, totVacc, peopVacc, totPeopVacc, dailyVacc, vaccines);
-					taskTwo(c);
-					break;
-				case 3:
-					Date sDate = dateFormat.parse(Console.getString("Enter first date in the format yyyy-MM-dd: "));
-					Date eDate = dateFormat.parse(Console.getString("Enter second date in the format yyyy-MM-dd: "));
-					isoCode = Console.getString("Enter ISO Code: ");
-					taskThree(sDate, eDate, isoCode);
-					break;
-				case 4:
-					int id = Console.getInt("Enter Record ID: ");
-					taskFour(id);
-					break;
+            //evaluate user choice
+            switch (choice) {
 
-			}//end switch
+                case 1:
+                    taskOne();
+                    break;
+                case 2:
+                    String country = Console.getString("Enter country: ");
+                    String isoCode = Console.getString("Enter ISO Code: ");
+                    Date now = new Date();
+                    String totalVaccinationsInput = Console.getString("Enter Total Vaccinations: ");
+                    int totalVaccinations = Console.getInt("Enter Total Vaccinations: ");
+                    int peopVacc = Console.getInt("Enter People Vaccinated: ");
+                    int totPeopVacc = Console.getInt("Enter People Fully Vaccinated: ");
+                    int dailyVacc = Console.getInt("Enter Daily Vaccinations: ");
+                    String vaccines = Console.getString("Enter Vaccines: ");
+                    CountryVaccinationData c = new CountryVaccinationData(choice, country, isoCode, now, totalVaccinations, peopVacc, totPeopVacc, dailyVacc, vaccines);
+                    taskTwo(c);
+                    break;
+                case 3:
+                    String beginDateInput = Console.getString("Enter first date in the format yyyy-MM-dd: ");
+                    if (beginDateInput.isBlank()) {
+                        beginDateInput = "2021-01-01";
+                        Console.displayLine("Defaulting to 2021-01-01");
+                    }
+                    Date beginDate = dateFormat.parse(beginDateInput);
+                    String endDateInput = Console.getString("Enter second date in the format yyyy-MM-dd: ");
+                    if (endDateInput.isBlank()) {
+                        endDateInput = "2024-08-30";
+                        System.out.println("Defaulting to 2024-08-30");
+                    }
+                    Date endDate = dateFormat.parse(endDateInput);
+                    isoCode = Console.getString("Enter ISO Code: ");
+                    if (isoCode.isBlank()) {
+                        isoCode = "IRE";
+                        System.out.println("Defaulting to IRE");
+                    }
+                    taskThree(beginDate, endDate, isoCode);
+                    break;
+                case 4:
+                    int id = Console.getInt("Enter Record ID: ");
+                    taskFour(id);
+                    break;
 
-			Console.displayLine();
+            }//end switch
 
-		}//end while
+            Console.displayLine();
 
-		//display ASCII art before exiting
-		ASCIIArtGenerator art = new ASCIIArtGenerator();
-		try
-		{
-			art.printTextArt("Bye!", 14, ASCIIArtGenerator.ASCIIArtFont.ART_FONT_DIALOG, "+");
-			System.exit(0);
-		}
-		catch(Exception ex)
-		{
-			Console.displayLine("Error with ASCII art " + ex);
-		}//end try
-	}
+        }//end while
 
-	private void taskOne(String iso_code) throws SQLException {
-		//ToDo Task 1
-		repo.searchByIsoCode(iso_code);
-		Console.displayLine();
-	}
+        //display ASCII art before exiting
+        ASCIIArtGenerator art = new ASCIIArtGenerator();
+        try {
+            art.printTextArt("Bye!", 14, ASCIIArtGenerator.ASCIIArtFont.ART_FONT_DIALOG, "+");
+            System.exit(0);
+        } catch (Exception ex) {
+            Console.displayLine("Error with ASCII art " + ex);
+        }//end try
+    }
 
-	private void taskTwo(CountryVaccinationData c) throws SQLException {
-		//ToDo Task 2
-		repo.insertNewRecord(c);
-		Console.displayLine();
-	}
+    private void taskOne() throws SQLException {
+        String isoCode = Console.getString("Enter ISO Code: ");
 
-	private void taskThree(Date sDate, Date eDate, String iso_code) throws SQLException {
-		//ToDo Task 3
-		repo.calculateAverageDailyVaccinations((java.sql.Date) sDate, (java.sql.Date) eDate, iso_code);
+        repository.searchByIsoCode(isoCode);
+        Console.displayLine();
+    }
 
-		Console.displayLine();
-	}
+    private void taskTwo(CountryVaccinationData c) throws SQLException {
+        //ToDo Task 2
+        repository.insertNewRecord(c);
+        Console.displayLine();
+    }
 
-	private void taskFour(int id) throws SQLException {
-		//ToDo Task 4
-		repo.retrieveRecordById(id);
-		Console.displayLine();
+    private void taskThree(Date sDate, Date eDate, String isoCode) throws SQLException {
+        repository.calculateAverageDailyVaccinations(new java.sql.Date(sDate.getTime()),
+                new java.sql.Date(eDate.getTime()),
+                isoCode);
 
-	}
+        Console.displayLine();
+    }
+
+    private void taskFour(int id) throws SQLException {
+        //ToDo Task 4
+        repository.retrieveRecordById(id);
+        Console.displayLine();
+
+    }
 
 }//end class
